1ea907c29bd1e8e6ac25b2af9cd9a6e4ff53eb04
Jens Titterness <jenstitterness@gmail.com>
2015-11-30 15:23:56 -0800

    updated application.js to play video. woohoo.

--- ExpediaTV/js/application.js
+++ ExpediaTV/js/application.js
@@ -12,7 +12,9 @@ App.onLaunch = function(options) {
 
     var showcase = createShowcase(res);
 
-    navigationDocument.presentModal(showcase);
+      playVideo();
+      
+//    navigationDocument.presentModal(showcase);
   };
 
   req.onreadystatechange = function() {
@@ -41,6 +43,20 @@ App.onLaunch = function(options) {
 };
 
 
+var playVideo = function() {
+    
+    var player = new Player();
+    
+    player.playlist = new Playlist();
+    
+    var video = new MediaItem('video', 'https://r3---sn-n2gpou-nx5e.googlevideo.com/videoplayback?expire=1448947091&lmt=1436806554288758&requiressl=yes&upn=dc51bNn4ZGw&sver=3&initcwndbps=4701250&mn=sn-n2gpou-nx5e&ipbits=0&mm=31&id=o-AEMQfsLaYIvDHXvMGu0JqxQN_n6K7F1Te-kvLjk7Ua-A&mv=m&dur=86.865&mt=1448925437&ms=au&ip=208.95.100.4&ratebypass=yes&signature=58253226F721D83751259F6E5CF07B85D79DCFDF.4081F0F6377D52BFFCFCC71CF52E69E6535FF4FA&itag=22&sparams=dur%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Cratebypass%2Crequiressl%2Csource%2Cupn%2Cexpire&key=yt6&fexp=9407051%2C9407535%2C9408710%2C9409245%2C9415515%2C9415792%2C9416126%2C9417683%2C9418203%2C9419838%2C9420452%2C9422436%2C9422596%2C9422618%2C9423162%2C9423662%2C9423724%2C9424196%2C9424446%2C9424963&source=youtube&pl=21&mime=video%2Fmp4');
+    
+    player.playlist.push(video);
+    player.play();
+    
+};
+
+
 var createShowcase = function(feed) {
   var showcase = `<?xml version="1.0" encoding="UTF-8" ?>
     <document>
@@ -48,7 +64,7 @@ var createShowcase = function(feed) {
    <banner>
       <title>Expedia</title>
       <row>
-         <button>…</button>
+         <button>...</button>
       </row>
    </banner>
    <carousel>
--- README.md
+++ README.md
@@ -1,9 +1,25 @@
 # expediaTV
 
 ## to run the server
-run this command in the folder that hosts the js files (ExpediaTV/js/)
+run this command in the folder that hosts the js files (ExpediaTV/)
 
 ```
 python -m SimpleHTTPServer 9001
 
 ```
+
+### Get youtube url
+to get the video urls install http://brew.sh/ and then run:
+
+```
+brew install youtube-dl 
+
+```
+
+once youtube-dl is installed you can run:
+
+```
+youtube-dl -g youtube_link_to_video | pbcopy
+```
+
+you can now paste the direct url to the youtube video. 
