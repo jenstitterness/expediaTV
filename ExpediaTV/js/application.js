e93f3aeeb6d489c3260069060b1dbcd661164521
Jens Titterness <jenstitterness@gmail.com>
2015-11-30 22:48:46 -0800

    videos load for the images as long as the links work. :/

--- ExpediaTV/js/application.js
+++ ExpediaTV/js/application.js
@@ -14,7 +14,7 @@ App.onLaunch = function(options) {
 
 //      playVideo();
       
-    navigationDocument.presentModal(showcase);
+    navigationDocument.pushDocument(showcase);
   };
 
   req.onreadystatechange = function() {
@@ -43,20 +43,6 @@ App.onLaunch = function(options) {
 };
 
 
-var playVideo = function() {
-    
-    var player = new Player();
-    
-    player.playlist = new Playlist();
-    
-    var video = new MediaItem('video', 'https://r3---sn-n2gpou-nx5e.googlevideo.com/videoplayback?expire=1448947091&lmt=1436806554288758&requiressl=yes&upn=dc51bNn4ZGw&sver=3&initcwndbps=4701250&mn=sn-n2gpou-nx5e&ipbits=0&mm=31&id=o-AEMQfsLaYIvDHXvMGu0JqxQN_n6K7F1Te-kvLjk7Ua-A&mv=m&dur=86.865&mt=1448925437&ms=au&ip=208.95.100.4&ratebypass=yes&signature=58253226F721D83751259F6E5CF07B85D79DCFDF.4081F0F6377D52BFFCFCC71CF52E69E6535FF4FA&itag=22&sparams=dur%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Cratebypass%2Crequiressl%2Csource%2Cupn%2Cexpire&key=yt6&fexp=9407051%2C9407535%2C9408710%2C9409245%2C9415515%2C9415792%2C9416126%2C9417683%2C9418203%2C9419838%2C9420452%2C9422436%2C9422596%2C9422618%2C9423162%2C9423662%2C9423724%2C9424196%2C9424446%2C9424963&source=youtube&pl=21&mime=video%2Fmp4');
-    
-    player.playlist.push(video);
-    player.play();
-    
-};
-
-
 var createShowcase = function(feed) {
   var showcase = `<?xml version="1.0" encoding="UTF-8" ?>
     <document>
@@ -74,8 +60,9 @@ var createShowcase = function(feed) {
 
     
     for (var i = 0; i < feed.destinations.length; i++) {
+        var youtubeURL = feed.destinations[i].youtubeURL;
         
-        showcase += `<lockup><img src="http://img.youtube.com/vi/`+ feed.destinations[i].youtubeId +`/maxresdefault.jpg" />`
+        showcase += `<lockup url="` + youtubeURL + `"><img src="http://img.youtube.com/vi/`+ feed.destinations[i].youtubeId +`/maxresdefault.jpg" />`
         showcase += `<description>`+feed.destinations[i].title+`</description>`
         showcase += `</lockup>`
     }
@@ -88,6 +75,29 @@ var createShowcase = function(feed) {
 
   var parser = new DOMParser();
   var showcaseDoc = parser.parseFromString(showcase, "application/xml");
+    
+    var lockups = showcaseDoc.getElementsByTagName("lockup");
+    
+
+    
+    for (var j = 0; j < lockups.length; j++) {
+        
+        lockups.item(j).addEventListener('select', function(evt) {
+                                var url = evt.target.getAttribute('url');
+
+                                
+                                var player = new Player();
+                                
+                                player.playlist = new Playlist();
+                                
+                                var video = new MediaItem('video', url);
+                                
+                                player.playlist.push(video);
+                                player.play();
+                                
+                                });
+    }
+    
   return showcaseDoc;
 
 };
