d3a7ff9d6e4a8e1642f0b16f6224c295d04bda08
Benjamin Rodgers <brodgers@BELC02NKA1XG3QC.sea.corp.expecn.com>
2015-12-01 15:48:28 -0800

    merge flight

--- ExpediaTV/js/application.js
+++ ExpediaTV/js/application.js
@@ -491,6 +491,37 @@ var unrealDealService = (function() {
     return instance;
 })();
 
+var flightService = (function() {
+                     
+    var instance = {
+        endPoint: "http://terminal2.expedia.com:80/x/mflights/search",
+        apiKey: "XAAYcpdWrOZCnGyMS5Wmtx06QMG9yWky",
+        offerCount: 10
+    };
+                     
+    instance.search = function(departureAirport, arrivalAirport, departureDate, returnDate, done, error) {
+        var params = {
+                "departureDate": instance.getISODateString(departureDate),
+                "returnDate": instance.getISODateString(returnDate),
+                "departureAirport": departureAirport,
+                "arrivalAirport": arrivalAirport,
+                "apikey": instance.apiKey,
+                "maxOfferCount": instance.offerCount
+            };
+        ajax.get(instance.endPoint, params, done, error);
+    };
+
+    instance.getISODateString = function(date) {
+        var outDate = date;
+        if(!(date instanceof Date)) {
+            outDate = new Date(date);
+        }
+        return outDate.toISOString().split("T")[0];
+    }
+                     
+    return instance;
+})();
+
 searchResults = (function(callback){
   var instance = {
     results: [],
